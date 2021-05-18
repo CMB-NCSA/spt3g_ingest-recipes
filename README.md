e2ce9d94dc2bd9286c212da5698c66d61295d380
Felipe Menanteau <felipe@illinois.edu>
2021-05-18 12:48:15 -0500

    Updates to build with TAG

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ git clone https://github.com/SouthPoleTelescope/spt3g_software.git
 
 # Build container
 export SPTUSER=$USER
-docker build -t spt3g_ubuntu --build-arg SPTUSER --rm=true .
+export TAG=`date +"%Y%B%d"`
+docker build -t spt3g_ubuntu:$TAG --build-arg SPTUSER --rm=true .
 ```
--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ git clone https://github.com/SouthPoleTelescope/spt3g_software.git
 
 # Build container
 export SPTUSER=$USER
-docker build -t spt3g_ubuntu --build-arg SPTUSER --rm=true .
+export TAG=`date +"%Y%B%d"`
+docker build -t menanteau/spt3g_ubuntu:$TAG --build-arg SPTUSER --rm=true .
