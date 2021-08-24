5ad6da56329039436ba6219c7b01ca91c9cd3b28
Felipe Menanteau <felipe@illinois.edu>
2021-08-24 10:34:43 -0500

    Adding centos7 Dockerfile

---
+++ build_centos7.sh
@@ -0,0 +1,8 @@
+# Clone locally
+#rm -rf spt3g_software
+#git clone https://github.com/SouthPoleTelescope/spt3g_software.git
+
+# Build container
+export SPTUSER=$USER
+export TAG=`date +"%Y%B%d"`
+docker build -f centos7/Dockerfile -t menanteau/spt3g_centos7:$TAG --build-arg SPTUSER --rm=true .
