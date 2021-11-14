cb0037990ab5d70c3086df0922d877ee2b98da57
Felipe Menanteau <felipe@illinois.edu>
2021-11-14 13:45:52 -0600

    Updates to software TAG and adding python3-magic via apt-get

--- ubuntu/setSPTEnv.sh
+++ ubuntu/setSPTEnv.sh
@@ -1,7 +1,11 @@
-#!/bin/bash 
+#!/usr/bin/env bash
+
+echo "Starting SPT3G environment"
 export SPT3G_SOFTWARE_PATH=/opt/spt/spt3g_software
 export SPT3G_SOFTWARE_BUILD_PATH=/opt/spt/spt3g_software/build
 export SPT3G_BUILD_ROOT=${SPT3G_SOFTWARE_BUILD_PATH}
 export PATH=${SPT3G_BUILD_ROOT}/bin:$PATH
 export LD_LIBRARY_PATH=${SPT3G_BUILD_ROOT}/spt3g:$LD_LIBRARY_PATH
 export PYTHONPATH=${SPT3G_BUILD_ROOT}:$PYTHONPATH
+
+/bin/bash --rcfile ~/.bashrc
