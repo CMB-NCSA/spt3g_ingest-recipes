98fc8f22776fe8b8baffc9d5121f167c8f521f09
Felipe Menanteau <felipe@illinois.edu>
2021-12-14 10:51:20 -0600

    Build update to gitlab

--- build_jupyter.sh
+++ build_jupyter.sh
@@ -4,12 +4,12 @@ cd ../
 
 # Build container
 export DISTRO=jupyter
-export IMAGE=spt3g_jupyter
+export IMAGE_REPO=registry.gitlab.com/spt3g/kubernetes/spt3g_jupyter
 export SPT3G_INGEST_VERSION=0.2.8
 export SPT3G_CUTTER_VERSION=0.3.0
 export TAG=${SPT3G_REV}_${SPT3G_INGEST_VERSION}_${SPT3G_CUTTER_VERSION}
 docker build -f $DISTRO/Dockerfile \
        --build-arg SPT3G_INGEST_VERSION \
        --build-arg SPT3G_CUTTER_VERSION \
-       -t menanteau/$IMAGE:$TAG \
+       -t $IMAGE_REPO:$TAG \
        --rm=true .
